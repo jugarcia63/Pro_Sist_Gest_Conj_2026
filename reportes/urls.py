a3ebf5ff3d48955041a212e412908a012496b9a2
juanes-CTRL <vilaaparicioj07@gmail.com>
2026-08-30 14:22:57 -0500

    Delete reportes directory

--- reportes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('crear/', views.CrearReporte.as_view(), name='crear_reporte'),
-    path('mis-reportes/', views.MisReportes.as_view(), name='mis_reportes'),
-    path('admin/reportes/', views.ListaReportesAdmin.as_view(), name='lista_reportes_admin'),
-    path('cambiar-estado/<int:pk>/', views.cambiar_estado, name='cambiar_estado_reporte'),
-]
