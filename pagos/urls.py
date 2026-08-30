153ad69817ff2c4b46679c9ff315efebf5cd0e5c
juanes-CTRL <vilaaparicioj07@gmail.com>
2026-08-30 14:22:47 -0500

    Delete pagos directory

--- pagos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('mis-pagos/', views.ListaPagosResidente.as_view(), name='lista_pagos_residente'),
-    path('admin/pagos/', views.ListaPagosAdmin.as_view(), name='lista_pagos_admin'),
-    path('crear/', views.CrearPago.as_view(), name='crear_pago'),
-    path('pagar/<int:pk>/', views.pasarela_pago, name='pasarela_pago'),
-]
