4b09395a079562a2a8ef14400ecf67d41af0f5bd
juanes-CTRL <vilaaparicioj07@gmail.com>
2026-08-30 14:24:25 -0500

    Delete vehiculos directory

--- vehiculos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ListaVehiculos.as_view(), name='lista_vehiculos'),
-    path('nuevo/', views.CrearVehiculo.as_view(), name='crear_vehiculo'),
-    path('editar/<int:id_vehiculo>/', views.EditarVehiculo.as_view(), name='editar_vehiculo'),
-]
