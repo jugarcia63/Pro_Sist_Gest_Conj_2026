b2352a58644be8df9c0959f59aa33b1763078c19
juanes-CTRL <vilaaparicioj07@gmail.com>
2026-08-30 14:24:35 -0500

    Delete visitantes directory

--- visitantes/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Visitantes
-
-admin.site.register(Visitantes)
--- visitantes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ListaVisitantes.as_view(), name='lista_visitantes'),
-    path('nuevo/', views.CrearVisitante.as_view(), name='crear_visitante'),
-    path('editar/<int:id_visitante>/', views.EditarVisitante.as_view(), name='editar_visitante'),
-]
